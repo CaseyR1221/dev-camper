30d20fbcc3488b8725ba1e5d8c5fd92993a080fd
Casey R <crowlandsdev@gmail.com>
2022-08-07 15:37:21 -0400

    created connection to mongoDB, added in mongoose, and added data files

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Require bootcamp rounts 
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to Database
+connectDB();
+
+// Require bootcamp rounts 
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -23,6 +28,13 @@ const PORT = process.env.PORT || 5001;
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.underline.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red.bold);
+    // stop the server && exit the application
+    server.close(() => process.exit(1));
+})
